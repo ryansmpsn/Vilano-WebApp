f01b34e02b0043d447b2f575ec82822063d08bf9
Ryan Simpson <ryan.simpson@postalfleetsvs.com>
2020-02-07 09:22:33 -0500

    Formatting

--- react-webapp/src/Routes.js
+++ react-webapp/src/Routes.js
@@ -1,23 +1,40 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import AppliedRoute, {ModuleRoute} from "./components/AppliedRoute";
+import AppliedRoute, { ModuleRoute } from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 
 import testpage from "./containers/testpage";
-import ContractList from "./containers/ContractList"
+import ContractList from "./containers/ContractList";
 
 export default function Routes({ appProps }) {
-    return (
-      <Switch>
-        <Redirect from="/" to="/home" exact component={Home} appProps={appProps} />
-        <AppliedRoute path="/home" exact component={Home} appProps={appProps} />
-        <ModuleRoute exact component={ContractList} appProps={appProps} cModule={appProps.contractAccess} path="/ContractList" />
-        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-        <AppliedRoute path="/testpage" exact component={testpage} appProps={appProps} />
-        { /* Finally, catch all unmatched routes */ }
-        <Route component={NotFound} />
-      </Switch>
-    );
+  return (
+    <Switch>
+      <Redirect
+        from="/"
+        to="/home"
+        exact
+        component={Home}
+        appProps={appProps}
+      />
+      <AppliedRoute path="/home" exact component={Home} appProps={appProps} />
+      <ModuleRoute
+        exact
+        component={ContractList}
+        appProps={appProps}
+        cModule={appProps.contractAccess}
+        path="/ContractList"
+      />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute
+        path="/testpage"
+        exact
+        component={testpage}
+        appProps={appProps}
+      />
+      {/* Finally, catch all unmatched routes */}
+      <Route component={NotFound} />
+    </Switch>
+  );
 }
--- react-webapp/src/components/EditModal.js
+++ react-webapp/src/components/EditModal.js
@@ -1,28 +1,31 @@
-import React  from "react";
-import { Button, Modal, FormGroup, FormControl, ControlLabel} from "react-bootstrap";
+import React from "react";
+import {
+  Button,
+  Modal,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
 import LoaderButton from "./LoaderButton";
 import Send from "./send";
 
-
 export default class EditModal extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       modalName: props.modalName,
       show: props.show,
       content: props.content,
       Permissions: props.accessLevel,
       editContent: props.content,
-      submitting: false,
-    }
+      submitting: false
+    };
   }
-  
-
 
   static getDerivedStateFromProps(props, state) {
     if (props.show !== state.show) {
       return {
-        show: props.show,
+        show: props.show
       };
     }
 
@@ -32,62 +35,69 @@ export default class EditModal extends React.Component {
 
   async handleSubmit(event) {
     event.preventDefault();
-    this.setState({submitting: true});
+    this.setState({ submitting: true });
     Send.post("/UpdateContract", this.state.editContent, this.props)
-    .then(res =>{
-      //bla bla update our contract to reflect DB. 
-      //TODO UPDATE CONTRACT TO REFLECT DB. 
-      console.log(res);
-    })
-    .error(err => {
-      console.log(err);
-    });
+      .then(res => {
+        //bla bla update our contract to reflect DB.
+        //TODO UPDATE CONTRACT TO REFLECT DB.
+        console.log(res);
+      })
+      .error(err => {
+        console.log(err);
+      });
     //.then()
   }
 
   render() {
-    return(
-      
+    return (
       <Modal show={this.state.show} onHide={this.props.closeModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>{this.state.modalName}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form onSubmit={this.handleSubmit.bind(this)}>
-              {this.state.editContent.map((item, index) =>
-                item[0] != "DONOTSHOW" &&
-                <FormGroup key={index}>
-                  <ControlLabel>{item[0] + ": " + item[1]} </ControlLabel>
-                  {/* Make this formcontrol tie to values for editing */}
-                  {this.state.Permissions === "Write" && (
-                    <FormControl type="text" 
-                    value={item[3]}
-                    onChange={(e) => {
-                      var object = this.state.editContent;
-                      var specials=/[*|\":<>[\]{}`\\()';@&$]/; //TODO setup global module to sanatize stuff.
-                      object[index][3] = e.target.value.replace(specials, "");
-                      this.setState({ editContent: object });
-                    }}
-                    placeholder={item[1]}/>)}
-                </FormGroup>
-              )}
-              {this.state.Permissions === "Write" && 
-              (<LoaderButton
-                  block
-                  type="submit"
-                  bsSize="large"
-                  //isLoading={this.state.submitting}
-                  disabled={this.state.submitting}
+        <Modal.Header closeButton>
+          <Modal.Title>{this.state.modalName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            {this.state.editContent.map(
+              (item, index) =>
+                item[0] != "DONOTSHOW" && (
+                  <FormGroup key={index}>
+                    <ControlLabel>{item[0] + ": " + item[1]} </ControlLabel>
+                    {/* Make this formcontrol tie to values for editing */}
+                    {this.state.Permissions === "Write" && (
+                      <FormControl
+                        type="text"
+                        value={item[3]}
+                        onChange={e => {
+                          var object = this.state.editContent;
+                          var specials = /[*|\":<>[\]{}`\\()';@&$]/; //TODO setup global module to sanatize stuff.
+                          object[index][3] = e.target.value.replace(
+                            specials,
+                            ""
+                          );
+                          this.setState({ editContent: object });
+                        }}
+                        placeholder={item[1]}
+                      />
+                    )}
+                  </FormGroup>
+                )
+            )}
+            {this.state.Permissions === "Write" && (
+              <LoaderButton
+                block
+                type="submit"
+                bsSize="large"
+                //isLoading={this.state.submitting}
+                disabled={this.state.submitting}
               >
-              Save
-              </LoaderButton>)}
-            </form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.props.closeModal}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-
+                Save
+              </LoaderButton>
+            )}
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.closeModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
