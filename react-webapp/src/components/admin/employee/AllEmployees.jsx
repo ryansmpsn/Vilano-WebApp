15960c670617c0a0fe511e063cc47d76f5c7819d
Ryan Simpson <ryan.simpson@postalfleetsvs.com>
2020-10-20 12:36:10 -0400

    Add new remote test

--- react-webapp/src/components/admin/employee/AllEmployees.jsx
+++ react-webapp/src/components/admin/employee/AllEmployees.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function AllEmployees(props) {
-  return <h1>Hello Employees Content</h1>;
+  return <h1>Hello Employee, Git Test</h1>;
 }
 
 export default AllEmployees;
