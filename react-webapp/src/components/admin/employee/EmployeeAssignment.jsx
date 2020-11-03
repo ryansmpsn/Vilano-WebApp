5d03df4deb06834ce064fb27dd064d3098298347
Ryan Simpson <ryan.simpson@postalfleetsvs.com>
2020-11-02 22:43:53 -0500

    Assign Employee to Contract

--- react-webapp/src/components/admin/employee/EmployeeAssignment.jsx
+++ react-webapp/src/components/admin/employee/EmployeeAssignment.jsx
@@ -3,38 +3,87 @@ import { Button, Card, Col, Row, Spinner } from "react-bootstrap";
 import Select from "react-select";
 import Send from "../../../libs/send";
 import { useNavigate } from "react-router";
+import { useToasts } from "react-toast-notifications";
 
 function EmployeeAssignment(props) {
   let navigate = useNavigate();
+  const { addToast } = useToasts();
 
   const [contractEmployees, setContractEmployees] = useState(null);
+  const [modifiedContractEmployees, setModifiedContractEmployees] = useState(null);
+  const [selectedContract, setSelectedContract] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   function handleContractSelect(x) {
+    setSelectedContract(x);
     setIsLoading(true);
     Send.get("/Employee/ContractEmployee/" + x.value).then((response) => {
       setIsLoading(false);
       setContractEmployees(response.data.value);
+      setModifiedContractEmployees(response.data.value);
     });
   }
 
   function handleEmployeeSelect(x) {
-    console.log(x);
+    let newEmployeeGroup = [...contractEmployees];
+    x &&
+      x.forEach((x) => {
+        let singleEmployee = [
+          { columnName: "employee_id", inputType: null, label: "Employee Id", value: null },
+          { columnName: "first_name", inputType: null, label: "First Name", value: null },
+          { columnName: "last_name", inputType: null, label: "Last Name", value: null },
+          { columnName: "contract_id", inputType: null, label: null, value: selectedContract.value },
+          { columnName: "external_contract_code", inputType: null, label: "Contract", value: selectedContract.label },
+          { columnName: "contract_role_id", inputType: null, label: null, value: null },
+          { columnName: "role", inputType: null, label: "Role", value: null },
+          { columnName: "is_primary", inputType: null, label: "Home Contract", value: null },
+          { columnName: "employee_contract_id", inputType: null, label: null, value: null },
+          { columnName: "is_active", inputType: null, label: "Active", value: true },
+          { columnName: "modified_timestamp", inputType: null, label: "Last Modified", value: null },
+          { columnName: "modified_by_employee_name", inputType: null, label: "Modified By", value: null },
+        ];
+        singleEmployee[0].value = x.value;
+        singleEmployee[1].value = x.label.split(" ")[0];
+        singleEmployee[2].value = x.label.split(" ")[1];
+
+        newEmployeeGroup.push(singleEmployee);
+      });
+    setModifiedContractEmployees(newEmployeeGroup);
   }
+
   function searchEmployee(x) {
     navigate("/administration/employee/" + x);
+  }
+
+  function saveEmployeeToContract() {
+    // Send.post("", modifiedContractEmployees).then((result) => {});
+    addToast(`Employees Saved to Contract: ${selectedContract.label}.`, {
+      appearance: "success",
+      autoDismiss: true,
+      autoDismissTimeout: 3000,
+    });
+    console.log(modifiedContractEmployees);
   }
 
   return (
     <>
       <Row className="mb-4 justify-content-md-center">
         <Col md="4">
-          <Select autofocus options={props.contractIds} placeholder={"Search for contracts by ID"} onChange={(x) => handleContractSelect(x)} />
+          <Select
+            autofocus
+            options={props.contractIds}
+            placeholder={"Search for contracts by ID"}
+            onChange={(x) => handleContractSelect(x)}
+          />
         </Col>
-        {console.log(props)}
         <Col md="4">
-          <Select isMulti options={props.employeeDropdowns[0].options} placeholder={"Employee List"} onChange={(x) => handleEmployeeSelect(x)} />
-          <Button className="btn btn-sm btn-outline-warning" onClick={()=> (console.log("Button was clicked"))}>Add</Button>
+          <Select
+            isMulti
+            options={props.employeeDropdowns[0].options || []}
+            placeholder={"Employee List"}
+            onChange={(x) => handleEmployeeSelect(x)}
+            isDisabled={contractEmployees === null}
+          />
         </Col>
       </Row>
       <Row>
@@ -49,10 +98,13 @@ function EmployeeAssignment(props) {
             <Spinner animation="border" variant="primary" />
           </Col>
         ) : (
-          contractEmployees !== null &&
-          contractEmployees.map((employee, index) => (
+          modifiedContractEmployees !== null &&
+          modifiedContractEmployees.map((employee, index) => (
             <Col md="3" key={"dataCol" + index}>
-              <Card border={employee[7].value === "true" ? "info" : "light"}>
+              <Card
+                className="mb-3"
+                border={employee[7].value === null ? "warning" : employee[7].value === "true" ? "info" : "light"}
+              >
                 <Card.Header>{employee[1].value + " " + employee[2].value}</Card.Header>
                 <Card.Body>
                   {employee.map(
@@ -66,13 +118,29 @@ function EmployeeAssignment(props) {
                 </Card.Body>
                 <Card.Footer>
                   <Button className="btn btn-sm" variant="outline-info" onClick={() => searchEmployee(employee[0].value)}>
-                    View Employee Data
+                    view Profile
+                  </Button>
+                  <Button
+                    className="btn btn-sm float-right"
+                    variant="outline-danger"
+                    onClick={() => console.log("remove this emp")}
+                  >
+                    remove
                   </Button>
                 </Card.Footer>
               </Card>
             </Col>
           ))
         )}
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="2">
+          {selectedContract && (
+            <Button className="btn btn-md" variant="outline-warning" onClick={() => saveEmployeeToContract()}>
+              save
+            </Button>
+          )}
+        </Col>
       </Row>
     </>
   );
